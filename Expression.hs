78d2b595bc710e5299faebd4a3d1fe9372b35b0d
wickedsnitch <romain.entakli@etu.univ-amu.fr>
2017-11-21 16:26:10 +0100

    Update Expression.hs

--- Expression.hs
+++ Expression.hs
@@ -1,4 +1,3 @@
-
 --https://kunigami.blog/2014/01/21/an-introduction-to-the-parsec-library/
 import Data.String
 
@@ -15,14 +14,15 @@ data Expression = Variable String
            | BinExpr BinOp Expression Expression 
            | UniExpr UniOp Expression
 
-type Store = [(Variable,Float)]
+type Store = [(String, Float)]
 
-store :: (Variable k) => k -> Store -> Maybe v
+store :: String -> Store -> Maybe Float
 store key [] = Nothing
 store key ((k,v):xs)
-    | key == x = Just v
-    | otherwise = Store key xs
+    | key == k = Just v
+    | otherwise = store key xs
 
-eval :: Store -> Expression ->Maybe Float
-eval (Float v)=> str v = v
-eval (Variable v)=> str v = store v str 
+eval :: Store -> Expression -> Maybe Float
+eval str (Const v) = Just v
+eval str (Variable v) = store v str
+eval str v = Nothing
