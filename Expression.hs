42f5f2fce975d2170c91e55372e666b268df4e4d
YshenorTheBis <31926943+YshenorTheBis@users.noreply.github.com>
2017-11-21 15:00:46 +0100

    Update Expression.hs

--- Expression.hs
+++ Expression.hs
@@ -1,24 +1,23 @@
 
 --https://kunigami.blog/2014/01/21/an-introduction-to-the-parsec-library/
+import Data.String
 
 
-type Variable = String
+data BinOp = Add
+           | Substract
+           | Multiply
+           | Exponent
 
-data B_Operator = Add Expression Expression
-           | Substract Expression Expression
-           | multiply Expression Expression
-           | exponent Expression Expression
+data UniOp = Minus
 
-data U_Operator = minus Expression
+data Expression = Variable String
+           | Const Float
+           | BinExpr BinOp Expression Expression 
+           | UniExpr UniOp Expression
 
-data Expression = Variable 
-           | Int 
-           | Expression `B_Operator` Expression 
-           | U_Operator Expression
+type Store = [(Variable,Float)]
 
-data Store = [(k,v)]
-
-store :: (Variable k) => k -> [(k, v)] -> Maybe v
+store :: (Variable k) => k -> Store -> Maybe v
 store key [] = Nothing
 store key ((k,v):xs)
     | key == x = Just v
